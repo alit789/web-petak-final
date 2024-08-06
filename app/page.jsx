e5fe28e2dfdffeaaee727bb4caf2c6ea5fbe1195
Ngakan Alit <alitwiradanta08@gmail.com>
2024-08-06 08:15:11 +0800

    update animation

--- app/page.jsx
+++ app/page.jsx
@@ -91,26 +91,28 @@ export default function Home() {
                 neque temporibus fugiat est?
               </p>
             </div>
-            <button className="btn btn-secondary mt-3 ">
-              Lets Explore{" "}
-              <span style={{ marginTop: "2px" }}>
-                <svg
-                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </span>
-            </button>
+            <div className="rv3">
+              <button className="btn btn-secondary mt-3 ">
+                Lets Explore{" "}
+                <span style={{ marginTop: "2px" }}>
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </span>
+              </button>
+            </div>
           </div>
           {/* hero Tagline */}
         </div>
